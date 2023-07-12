d18e1114b02a4dddb2accf5cbf2cd0513b71e808
JJS <joel.steiner@unifr.ch>
2023-07-12 16:55:24 +0200

    adapted output reader

--- Algo/test.jl
+++ Algo/test.jl
@@ -1,7 +1,7 @@
 include("IPB_Batching.jl")
 
 
-fileName = "Data/Instances_txt/inst_200_10_4.txt"
+fileName = "Data/Instances_txt/inst_100_10_4.txt"
 
 println("Read data from file: ", fileName)
 first_row, N = read_data(fileName)
@@ -21,6 +21,10 @@ c = compute_cikB(A, N)
 
 println("Create model")
 x, flow_conservation_constraints, partitioning_constraints, model = create_model(A,c, N)
+
+parts = split(fileName, "/")
+instance_name = parts[end]
+output_filename = "Output/MILP_$(instance_name)_$(timestamp).txt"
 configure_gurobi_logging(output_filename, model)
 
 println("Solve model")
